051e4d51a8d78d6bb1e605e0e3e4999ec417c797
isha-bagadiya <ishabagadiya@lampos.tech>
2024-09-30 09:49:29 +0530

    animation added

--- src/app/components/homepage components/Homepage.js
+++ src/app/components/homepage components/Homepage.js
@@ -7,54 +7,115 @@ import img2 from "../../../../public/Illustration2.svg";
 import img3 from "../../../../public/Illustration3.svg";
 import img4 from "../../../../public/Illustration4.svg";
 import Link from "next/link";
-import { useWallet } from '@tronweb3/tronwallet-adapter-react-hooks';
+import { useWallet } from "@tronweb3/tronwallet-adapter-react-hooks";
 import { WalletActionButton } from "@tronweb3/tronwallet-adapter-react-ui";
+import { motion } from "framer-motion";
 
 const Homepage = () => {
   const { connected } = useWallet();
 
+  const fadeInUp = {
+    initial: { opacity: 0, y: 60 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 1 },
+  };
+
+  const staggerChildren = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const glowAnimation = {
+    initial: { textShadow: "0 0 0px rgba(255,255,255,0)" },
+    animate: {
+      textShadow: [
+        "0 0 4px rgba(255,255,255,0.1)",
+        "0 0 10px rgba(255,255,255,0.3)",
+        "0 0 16px rgba(255,255,255,0.6)",
+        "0 0 20px rgba(255,255,255,0.3)",
+        "0 0 4px rgba(255,255,255,0.1)",
+      ],
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+        repeatType: "reverse",
+      },
+    },
+  };
+
+  const rotateAnimation = {
+    initial: { rotate: 0 },
+    animate: {
+      rotate: [0, 20, 0, -20, 0],
+      transition: {
+        duration: 100,
+        ease: "easeInOut",
+        times: [0, 0.25, 0.5, 0.75, 1],
+        repeat: Infinity,
+      },
+    },
+  };
+
   return (
-    <div className="w-[90%] mx-auto px-16 font-regular-actay ">
+    <div className="w-[90%] mx-auto px-16 font-regular-actay">
       <div className="w-full flex flex-col">
         <div className="flex flex-col items-center mt-10 h-[75vh] overflow-hidden">
           <div className="w-full flex flex-col items-center">
             <h1 className="text-6xl text-center w-[90%] mx-auto leading-tight">
-              <span className="bg-tronique bg-clip-text text-transparent">
+              <span className="bg-tronique bg-clip-text text-transparent inline-block">
                 TroniQue:
               </span>{" "}
-              Simplifying Tron and Empowering Users with TroniQue
+              <span>Simplifying Tron and Empowering Users with TroniQue</span>
             </h1>
-            <p className="text-xl text-center mt-5">
+            <p className="text-xl text-center mt-5" variants={fadeInUp}>
               Navigate the world of TRON with unmatched ease and insight
             </p>
 
             {!connected && (
-              <div className="bg-tronique rounded-full overflow-hidden p-[1px] w-[140px] h-[50px] relative z-20 flex items-center justify-center my-6">
+              <motion.div
+                className="bg-tronique rounded-full overflow-hidden p-[1px] w-[140px] h-[50px] relative z-20 flex items-center justify-center my-6"
+                variants={fadeInUp}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
                 <div className="bg-[#621D1D] w-full h-full rounded-full hover:bg-[#621d1da1]">
-                <WalletActionButton>Get Started</WalletActionButton>
+                  <WalletActionButton>Get Started</WalletActionButton>
                 </div>
-              </div>
+              </motion.div>
             )}
             {connected && (
               <Link href="/forum">
-                <div className="bg-tronique rounded-full overflow-hidden p-[1px] w-[140px] h-[50px] relative z-20 flex items-center justify-center my-6">
+                <motion.div
+                  className="bg-tronique rounded-full overflow-hidden p-[1px] w-[140px] h-[50px] relative z-20 flex items-center justify-center my-6"
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                >
                   <div className="bg-[#621D1D] w-full h-full rounded-full hover:bg-[#621d1da1]">
                     <button className="z-20 relative rounded-full w-full h-full bg-gradient-to-b from-[#621d1d52] to-[#DE082D4D] font-light">
                       Get Started
                     </button>
                   </div>
-                </div>
+                </motion.div>
               </Link>
             )}
           </div>
-          <div className="w-full h-max -mt-36 z-10 relative">
-            <Image
-              src={img1}
-              alt="img1"
-              className="w-[75%] h-auto mx-auto"
-              priority
-            ></Image>
-          </div>
+          <motion.div className="w-full h-max -mt-36 z-10 relative">
+            <motion.div
+              variants={rotateAnimation}
+              initial="initial"
+              animate="animate"
+            >
+              <Image
+                src={img1}
+                alt="img1"
+                className="w-[75%] h-auto mx-auto"
+                priority
+              />
+            </motion.div>
+          </motion.div>
         </div>
 
         <div className="flex items-center justify-between p-10 py-14 bg-gradient-to-b from-[#242424f0] to-[#1d1a1afb] rounded-2xl w-full mb-10">
@@ -69,13 +130,6 @@ const Homepage = () => {
               analyze conversations to stay on top of the latest topics,
               community opinions, and key discussions.
             </p>
-            {/* {connected && (
-              <Link href="/forum">
-                <button className="px-6 py-2 rounded-full border border-white w-max mt-4">
-                  Get Started
-                </button>
-              </Link>
-            )} */}
           </div>
           <div className="w-[33%] h-max">
             <Image
@@ -100,14 +154,6 @@ const Homepage = () => {
               energy usage, and analyze trade patterns to make informed
               decisions.
             </p>
-
-            {/* {connected && (
-              <Link href="/dex-trades">
-                <button className="px-6 py-2 rounded-full border border-white w-max mt-4">
-                  Get Started
-                </button>
-              </Link>
-            )} */}
           </div>
           <div className="w-[33%] h-max">
             <Image
@@ -135,11 +181,6 @@ const Homepage = () => {
               knowledgeable guide right at your fingertips, making sure you get
               the precise data you need without the usual hassle.
             </p>
-            {/* {connected && (
-              <button className="px-6 py-2 rounded-full border border-white w-max mt-4">
-                Get Started
-              </button>
-            )} */}
           </div>
           <div className="w-[33%] h-max">
             <Image
